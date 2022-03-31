6bcf73516c24d93b4a85a7c3993850f9f1dab825
Sukhjinder Arora <35064725+SukhjinderArora@users.noreply.github.com>
2022-03-31 16:23:58 +0530

    changed sequelize db authentication method to support heroku postgres service

--- utils/database.js
+++ utils/database.js
@@ -11,5 +11,15 @@ const { Sequelize } = require('sequelize');
 //   }
 // );
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
 module.exports = sequelize;
